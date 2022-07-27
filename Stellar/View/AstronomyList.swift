7ed83f184c8bcaba3c4292f48141c2870f9b0b22
Harry-KNIGHT <eliott.knight@icloud.com>
2022-07-27 18:40:54 +0200

    Refreshable action,

--- Stellar/View/AstronomyList.swift
+++ Stellar/View/AstronomyList.swift
@@ -13,18 +13,17 @@ struct AstronomyList: View {
 	@State private var showSheet = false
 	@State private var showLoadingIndicator = true
 	var linewidth: CGFloat = 5
-    var body: some View {
+	var body: some View {
 		NavigationView {
 			VStack {
-			if astronomiesApi.allAstronomies.isEmpty {
-				Text("Stellar is loading data...")
-					.font(.title3.bold())
-					.foregroundStyle(.primary)
-				ActivityIndicatorView(isVisible: $showLoadingIndicator, type: .growingArc(lineWidth: linewidth))
-					.foregroundColor(.red)
-					.frame(width: 50.0, height: 50.0)
-			}else {
-
+				if astronomiesApi.allAstronomies.isEmpty {
+					Text("Stellar is loading data...")
+						.font(.title3.bold())
+						.foregroundStyle(.primary)
+					ActivityIndicatorView(isVisible: $showLoadingIndicator, type: .growingArc(lineWidth: linewidth))
+						.foregroundColor(.red)
+						.frame(width: 50.0, height: 50.0)
+				}else {
 					List {
 						ForEach(astronomiesApi.allAstronomies.reversed(), id: \.date) { astronomy in
 							NavigationLink(destination: AstronomyDetailView(astronomyObject: astronomy)) {
@@ -33,7 +32,8 @@ struct AstronomyList: View {
 						}
 					}
 				}
-			}.navigationTitle("Stellar")
+			}
+			.navigationTitle("Stellar")
 			.toolbar {
 				ToolbarItem(placement: .navigationBarTrailing) {
 					Button(action: {
@@ -48,20 +48,22 @@ struct AstronomyList: View {
 					}
 				}
 			}
+		}.refreshable {
+			Task {
+				astronomiesApi.allAstronomies
+			}
 		}
 		.task {
 			await astronomiesApi.fetchAstronomiesObject(to: Date.now)
-			  }
-    }
-
+		}
+	}
 }
 
 struct AstronomyList_Previews: PreviewProvider {
-    static var previews: some View {
-        AstronomyList()
+	static var previews: some View {
+		AstronomyList()
 			.environmentObject(AllAstronomiesApi())
-
-    }
+	}
 }
 
 struct ListRowCell: View {
