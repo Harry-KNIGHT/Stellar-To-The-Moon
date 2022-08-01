f7da3b6e01280e3cfb12895b69701a8016eabfd1
Harry-KNIGHT <eliott.knight@icloud.com>
2022-08-01 14:23:11 +0200

    Person in space row UI.

--- Stellar/View/MainViews/PersonInSpaceView.swift
+++ Stellar/View/MainViews/PersonInSpaceView.swift
@@ -20,52 +20,25 @@ struct PersonInSpaceView: View {
 									RoundedRectangle(cornerRadius: 10)
 										.frame(maxWidth: .infinity, maxHeight: .infinity)
 										.foregroundStyle(.regularMaterial)
+									if person.agency.lowercased() == "nasa" {
 
-									HStack {
-										if person.agency.lowercased() == "nasa" {
-											PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position)
-											Spacer()
-											Image("NASAL")
-												.resizable()
-												.scaledToFit()
-												.frame(height: 60)
+										PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position, image: "NASAL")
 
+									} else if person.agency.lowercased() == "roscosmos" {
 
+										PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position, image: "ROSCOSMOSL")
 
-										} else if person.agency.lowercased() == "roscosmos" {
+									} else if person.agency.lowercased() == "esa" {
 
+										PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position, image: "ESAL", imageHeight: 30)
 
-											PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position)
-											Spacer()
-											Image("ROSCOSMOSL")
-												.resizable()
-												.scaledToFit()
-												.frame(height: 60)
-										} else if person.agency.lowercased() == "esa" {
+									} else if person.agency.lowercased() == "cnsa" {
 
-											PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position)
-											Spacer()
-											Image("ESAL")
-												.resizable()
-												.scaledToFit()
-												.frame(height: 60)
+										PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position, image: "CNSAL", corner: 10)
 
-
-										} else if person.agency.lowercased() == "cnsa" {
-											PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position)
-											Spacer()
-											Image("CNSAL")
-												.resizable()
-												.scaledToFit()
-												.frame(height: 60)
-
-
-										} else {
-											PersonInSpaceRow(name: person.name, agency: person.agency, position: person.position)
-										}
-
-
-									}.padding(.horizontal)
+									} else {
+										PersonInSpaceRow(hasGotImage: false, name: person.name, agency: person.agency, position: person.position, image: "")
+									}
 								}
 							}
 						}
@@ -94,20 +67,48 @@ struct PersonInSpaceView_Previews: PreviewProvider {
 }
 
 struct PersonInSpaceRow: View {
+	var hasGotImage: Bool = true
 	var name: String
 	var agency: String
 	var position: String
+	var image: String
+	var imageHeight: CGFloat = 60
+	var corner: CGFloat = 0
+
 	var body: some View {
-		VStack(alignment: .leading) {
-			Text(name)
-				.font(.title3)
-				.foregroundColor(.primary)
-			Text(agency)
-				.foregroundColor(.primary)
-				.font(.headline)
-			Text(position)
-				.font(.subheadline)
-				.foregroundColor(.secondary)
-		}.padding(.vertical)
-	}
+		if hasGotImage {
+			HStack {
+				VStack(alignment: .leading) {
+					Text(name)
+						.font(.title3)
+						.foregroundColor(.primary)
+					Text(agency)
+						.foregroundColor(.primary)
+						.font(.headline)
+					Text(position)
+						.font(.subheadline)
+						.foregroundColor(.secondary)
+				}.padding(.vertical)
+				Spacer()
+				Image(image)
+					.resizable()
+					.scaledToFit()
+					.frame(height: imageHeight)
+					.clipShape(RoundedRectangle(cornerRadius: corner, style: .continuous))
+			}.padding(.horizontal, 8)
+		}else {
+				VStack(alignment: .leading) {
+					Text(name)
+						.font(.title3)
+						.foregroundColor(.primary)
+					Text(agency)
+						.foregroundColor(.primary)
+						.font(.headline)
+					Text(position)
+						.font(.subheadline)
+						.foregroundColor(.secondary)
+				}
+				.padding()
+			}
+		}
 }
