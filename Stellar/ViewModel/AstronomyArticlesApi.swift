e493bf1181678897d179009ce4312c9bdff3a57f
Harry-KNIGHT <eliott.knight@icloud.com>
2022-08-29 17:59:37 +0200

    Add userDefault for keep astronomy articles if user close the app.

--- Stellar/ViewModel/AstronomyArticlesApi.swift
+++ Stellar/ViewModel/AstronomyArticlesApi.swift
@@ -9,12 +9,29 @@ import SwiftUI
 import StellarMoonKit
 
 class AstronomiesArticleViewModel: ObservableObject {
-	@Published var allAstronomies = [NasaAstronomyResponse]()
+	@Published var allAstronomies: [NasaAstronomyResponse]
 
+	init() {
+		if let data = UserDefaults.standard.data(forKey: "SavedData") {
+			if let decoded = try? JSONDecoder().decode([NasaAstronomyResponse].self, from: data) {
+				allAstronomies = decoded
+				return
+			}
+		}
+		// No Saved data
+		allAstronomies = []
+	}
+
+	func save() {
+		if let encoded = try? JSONEncoder().encode(allAstronomies) {
+			UserDefaults.standard.set(encoded, forKey: "SavedData")
+		}
+	}
 
 	@MainActor func getAstronomiesArticles(from hundredDayBefore: Int64 = Date().millisecondsSince1970 , to today: Date) async throws {
 		do {
 			allAstronomies = try await AstronomiesArticleApi.fetchAstronomiesObject(from: hundredDayBefore, to: today)
+			save()
 		} catch {
 			print("Error \(error.localizedDescription)")
 		}
