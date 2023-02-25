cfafd62055e09519469f71f0a4f4613778b0f1ab
Harry-KNIGHT <eliott.knight@icloud.com>
2023-02-25 20:27:43 +0100

    Setup notification with onesignal sdk

--- Stellar/NasaDayAstronomyApp.swift
+++ Stellar/NasaDayAstronomyApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OneSignal
 
 @main
 struct NasaDayAstronomyApp: App {
@@ -13,6 +14,8 @@ struct NasaDayAstronomyApp: App {
 	@StateObject var articlesVM = ArticleViewModel()
 	@StateObject var favoriteVM = FavoriteViewModel()
 	@StateObject var searchDateArticleVM = SearchDateArticleViewModel()
+
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
@@ -23,4 +26,21 @@ struct NasaDayAstronomyApp: App {
 				.environmentObject(searchDateArticleVM)
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+	   // Remove this method to stop OneSignal Debugging
+	   OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+
+	   OneSignal.initWithLaunchOptions(launchOptions)
+	   OneSignal.setAppId("6b80052a-a147-45bb-bfc3-de331f2c33f6")
+
+	   OneSignal.promptForPushNotifications(userResponse: { accepted in
+		 print("User accepted notification: \(accepted)")
+	   })
+	  // Set your customer userId
+	  // OneSignal.setExternalUserId("userId")
+	   return true
+	}
 }
---
+++ Stellar/NotificationService.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationService.swift
+//  Stellar
+//
+//  Created by Elliot Knight on 25/02/2023.
+//
+import UserNotifications
+
+import OneSignal
+
+class NotificationService: UNNotificationServiceExtension {
+
+	var contentHandler: ((UNNotificationContent) -> Void)?
+	var receivedRequest: UNNotificationRequest!
+	var bestAttemptContent: UNMutableNotificationContent?
+
+	override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+		self.receivedRequest = request
+		self.contentHandler = contentHandler
+		self.bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+
+		if let bestAttemptContent = bestAttemptContent {
+			OneSignal.didReceiveNotificationExtensionRequest(self.receivedRequest, with: bestAttemptContent, withContentHandler: self.contentHandler)
+		}
+	}
+
+	override func serviceExtensionTimeWillExpire() {
+		// Called just before the extension will be terminated by the system.
+		// Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+		if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+			OneSignal.serviceExtensionTimeWillExpireRequest(self.receivedRequest, with: self.bestAttemptContent)
+			contentHandler(bestAttemptContent)
+		}
+	}
+}
