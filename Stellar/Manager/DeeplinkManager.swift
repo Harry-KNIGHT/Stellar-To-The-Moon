166f18210419bbe7390a1944dd7164691b8dd8c9
Harry-KNIGHT <eliott.knight@icloud.com>
2023-04-10 15:15:05 +0200

    Add deeplinkManager

---
+++ Stellar/Manager/DeeplinkManager.swift
@@ -0,0 +1,21 @@
+//
+//  DeeplinkManager.swift
+//  Stellar
+//
+//  Created by Elliot Knight on 10/04/2023.
+//
+
+import Foundation
+
+class DeepLinkManager: ObservableObject {
+	@Published var selectedArticleID: String?
+
+	func handleDeepLink(url: URL) {
+		if let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
+		   components.scheme == "myapp",
+		   components.host == "article",
+		   let articleID = components.path?.replacingOccurrences(of: "/", with: "") {
+			selectedArticleID = articleID
+		}
+	}
+}
