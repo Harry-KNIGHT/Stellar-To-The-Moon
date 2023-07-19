878a1f8bb2779ccf8750f1b7486b88ae0ae5cd14
Harry-KNIGHT <eliott.knight@icloud.com>
2023-07-19 09:22:36 +0200

    feat: Add domain layer and move vm into folders and add application folder

---
+++ Domain/Sources/Domain/Repository/Articles/ArticleRepositoryDefault.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleRepositoryDefault.swift
+//  
+//
+//  Created by Elliot Knight on 19/07/2023.
+//
+
+import Foundation
+import StellarMoonKit
+
+final class ArticleRepositoryDefault: ArticleRepository {
+	func getArticles() async throws -> [Article] {
+		// TODO: Move fetching api call here
+		return []
+	}
+}
