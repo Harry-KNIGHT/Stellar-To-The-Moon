eba63964452ade9c102a254bea9807bf30e8b75f
Harry-KNIGHT <eliott.knight@icloud.com>
2023-07-26 14:52:54 +0200

    feat: Download image error handling enum

--- Stellar/Presentation/ArticlesList/Detail/ViewModel/DownloadImageViewModel.swift
+++ Stellar/Presentation/ArticlesList/Detail/ViewModel/DownloadImageViewModel.swift
@@ -9,22 +9,25 @@ import Foundation
 import UIKit
 import Domain
 
-class DownloadImageViewModel: ObservableObject {
-	func downloadImageToCameraRoll(from url: String) async throws -> UIImage {
+final class DownloadImageViewModel: ObservableObject {
+	public func downloadImageToCameraRoll(from url: String) async throws -> UIImage {
 		guard let url = URL(string: url) else {
-//			throw ApiError.urlNotFound
-			return UIImage.checkmark
+			throw DownloadImageError.urlNotFound
 		}
 		do {
 			let (data, _) = try await URLSession.shared.data(from: url)
 			if let image = UIImage(data: data) {
 				return image
 			} else {
-//				throw ApiError.noDataForImage
-				return UIImage.checkmark
+				throw DownloadImageError.emptyImageData
 			}
 		} catch {
 			return UIImage.checkmark
 		}
 	}
+}
+
+enum DownloadImageError: Error {
+	case urlNotFound
+	case emptyImageData
 }
