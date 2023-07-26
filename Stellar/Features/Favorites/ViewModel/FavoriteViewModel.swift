3c21b98504521f9b0ad84df9e1d90764c6e8a285
Harry-KNIGHT <eliott.knight@icloud.com>
2023-07-26 17:43:45 +0200

    refac: Add marks and move down private methods

--- Stellar/Features/Favorites/ViewModel/FavoriteViewModel.swift
+++ Stellar/Features/Favorites/ViewModel/FavoriteViewModel.swift
@@ -11,6 +11,8 @@ import Domain
 class FavoriteViewModel: ObservableObject {
 	@Published var favoriteArticles: [Article]
 
+	// MARK: Initializer
+	
 	init() {
 		if let data = UserDefaults.standard.data(forKey: "SavedFavoritesData") {
 			if let decoded = try? JSONDecoder().decode([Article].self, from: data) {
@@ -22,39 +24,7 @@ class FavoriteViewModel: ObservableObject {
 		favoriteArticles = []
 	}
 
-	private func save() {
-		if let encoded = try? JSONEncoder().encode(favoriteArticles) {
-			UserDefaults.standard.set(encoded, forKey: "SavedFavoritesData")
-		}
-	}
-
-	/// Add article to favorite
-	/// - Parameter article: Article is from NasaAstronomy model
-	private func addToFavorite(article: Article) {
-		self.favoriteArticles.insert(article, at: 0)
-	}
-
-	/// Delet Favorite from list swipe
-	/// - Parameter offsets: offset permit to delet article with swipe gesture
-	func deletFavorite(at offsets: IndexSet) {
-		favoriteArticles.remove(atOffsets: offsets)
-		save()
-	}
-
-	/// Delet selected article
-	/// - Parameter article: Article is from NasaAstronomy model
-	private func deletSelectedFavorite(article: Article) {
-		self.favoriteArticles.removeAll { $0.date == article.date }
-	}
-
-	/// Permit to move index of each article in the array manually.
-	/// - Parameters:
-	///   - index: From current position
-	///   - offset: To final position
-	func moveFavorite(from index: IndexSet, to offset: Int) {
-		self.favoriteArticles.move(fromOffsets: index, toOffset: offset)
-		save()
-	}
+	// MARK: Public methods
 
 	/// Add or delet article if it's on array yet or no.
 	/// - Parameter article: Picture, description and copyright.
@@ -68,12 +38,47 @@ class FavoriteViewModel: ObservableObject {
 		}
 	}
 
-
 	func isArticleIsInFavorites(_ article: Article) -> Bool {
 		return self.favoriteArticles.contains(article)
 	}
 
 	func setFavoriteIcon(article: Article) -> String {
 		return isArticleIsInFavorites(article) ? "star.fill" : "star"
+	}
+
+	/// Permit to move index of each article in the array manually.
+	/// - Parameters:
+	///   - index: From current position
+	///   - offset: To final position
+	func moveFavorite(from index: IndexSet, to offset: Int) {
+		self.favoriteArticles.move(fromOffsets: index, toOffset: offset)
+		save()
+	}
+
+	/// Delet Favorite from list swipe
+	/// - Parameter offsets: offset permit to delet article with swipe gesture
+	func deletFavorite(at offsets: IndexSet) {
+		favoriteArticles.remove(atOffsets: offsets)
+		save()
+	}
+
+	// MARK: Private methods
+
+	private func save() {
+		if let encoded = try? JSONEncoder().encode(favoriteArticles) {
+			UserDefaults.standard.set(encoded, forKey: "SavedFavoritesData")
+		}
+	}
+
+	/// Add article to favorite
+	/// - Parameter article: Article is from NasaAstronomy model
+	private func addToFavorite(article: Article) {
+		self.favoriteArticles.insert(article, at: 0)
+	}
+
+	/// Delet selected article
+	/// - Parameter article: Article is from NasaAstronomy model
+	private func deletSelectedFavorite(article: Article) {
+		self.favoriteArticles.removeAll { $0.date == article.date }
 	}
 }
